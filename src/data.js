22b952843b97c7bf1ede2104dcbb76a35a5ff6d9
kumhon95 <loke_kumhon@yahoo.com.sg>
2021-05-09 23:55:39 +0800

    first commit

---
+++ src/data.js
@@ -0,0 +1,168 @@
+export const storeProducts = [
+    {
+        id: 1,
+        title: "Captain America",
+        img: "img/toy1.jpg",
+        price: 80.00,
+        brand: "Marvel",
+        info: "He is America's first super solider, fighting fearlessly on the side of justice.",
+        inCart: false,
+        count: 0,
+        total: 0
+    },
+    {
+        id: 2,
+        title: "Hulk",
+        img: "img/toy2.jpg",
+        price: 19.90,
+        brand: "Marvel",
+        info: "Little heroes can create big adventures in their favorite world",
+        inCart: false,
+        count: 0,
+        total: 0
+    },
+    {
+        id: 3,
+        title: "Thor",
+        img: "img/toy3.jpg",
+        price: 75.00,
+        brand: "Marvel",
+        info: "Thor using the mighty strength to protect worthy allies in battle.",
+        intoCart: false,
+        count: 0,
+        total: 0
+    },
+    {
+        id: 4,
+        title: "Black Widow",
+        img: "img/toy4.jpg",
+        price: 40.00,
+        brand: "Marvel",
+        info: "Authentic and detailed fully realized likeness of Black Widow.",
+        intoCart: false,
+        count: 0,
+        total: 0
+    },
+    {
+        id: 5,
+        title: "Iron Man",
+        img: "img/toy5.jpg",
+        price: 89.90,
+        brand: "Marvel",
+        info: "This Iron Man figure includes a blast effect accessory.",
+        intoCart: false,
+        count: 0,
+        total: 0
+    },
+    {
+        id: 6,
+        title: "Wolverine",
+        img: "img/toy6.jpg",
+        price: 55.00,
+        brand: "XMen",
+        info: "Wolverine is a mutant and a member of the X-Men.",
+        intoCart: false,
+        count: 0,
+        total: 0
+    },
+    {
+        id: 7,
+        title: "Storm",
+        img: "img/toy7.jpg",
+        price: 45.00,
+        brand: "XMen",
+        info: "Storm is the descendent of an ancient line of African priestesses.",
+        intoCart: false,
+        count: 0,
+        total: 0
+    },
+    {
+        id: 8,
+        title: "Cyclops",
+        img: "img/toy8.jpg",
+        price: 60.00,
+        brand: "XMen",
+        info: "Cyclops can project a beam of ruby-colored concussive force from his eyes.",
+        intoCart: false,
+        count: 0,
+        total: 0
+    },
+    {
+        id: 9,
+        title: "Rogue",
+        img: "img/toy9.jpg",
+        price: 40.00,
+        brand: "XMen",
+        info: "Rogue is a fictional superhero appearing in American comic books published by Marvel Comics, commonly in association with the X-Men.",
+        intoCart: false,
+        count: 0,
+        total: 0
+    },
+    {
+        id: 10,
+        title: "Beast",
+        img: "img/toy10.jpg",
+        price: 55.00,
+        brand: "XMen",
+        info: "Beast is a mutant that has the ability to transform into a blue skinned creature with claws and fangs.",
+        intoCart: false,
+        count: 0,
+        total: 0
+    },
+    {
+        id: 11,
+        title: "Mr Fantastic",
+        img: "img/toy11.jpg",
+        price: 60.00,
+        brand: "Fantastic 4",
+        info: "Mr Fantastic can stretch his body to great distance.",
+        intoCart: false,
+        count: 0,
+        total: 0
+    },
+    {
+        id: 12,
+        title: "Human Torch",
+        img: "img/toy12.jpg",
+        price: 60.00,
+        brand: "Fantastic 4",
+        info: "Like the rest of the Fantastic Four, Jonathan Johnny Storm gained his powers on a spacecraft bombarded by cosmic rays.",
+        intoCart: false,
+        count: 0,
+        total: 0
+    },
+    {
+        id: 13,
+        title: "Invincible Woman",
+        img: "img/toy13.jpg",
+        price: 42.00,
+        brand: "Fantastic 4",
+        info: "The formidable heroine Invisible Woman uses her powers of invisibility to protect Earth and acts as the leading maternal figure of the Fantastic Four.",
+        intoCart: false,
+        count: 0,
+        total: 0
+    },
+    {
+        id: 14,
+        title: "The Thing",
+        img: "img/toy14.jpg",
+        price: 60.00,
+        brand: "Fantastic 4",
+        info: "Big Grimm is the original quintessential tough guy of the Marvel-Universe.",
+        intoCart: false,
+        count: 0,
+        total: 0
+    },
+];
+
+export const detailProduct = {
+    id: 1,
+    title: "Captain America",
+    img: "img/toy1.jpg",
+    price: 80.00,
+    brand: "Marvel",
+    info: "He is America's first super solider, fighting fearlessly on the side of justice.",
+    inCart: false,
+    count: 0,
+    total: 0
+};
